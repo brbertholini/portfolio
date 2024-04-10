6f9fabe7ba6bc1ccadab9d350a639af81e60746d
Bruno Bertholini <brbertholini@gmail.com>
2024-04-10 15:00:58 -0300

    Creating project

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <div className="flex justify-center max-w-(1240px) px4 items-center h-24">
+            <ul className="flex">
+                <li className="text-white p-4">projetos</li>
+                <li className="text-white p-4">contato</li>
+            </ul>
+        </div>
+    )
+}
