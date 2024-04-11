e4ec1f10878578ca0bd0258e93555505693687d9
Bruno Bertholini <brbertholini@gmail.com>
2024-04-10 21:57:48 -0300

    FEAT: Starting responsive adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import { Header } from "./components/Header"
-import { SideIcons } from "./components/SideIcons"
+import { Project } from "./components/Project"
 
 function App() {
 
   return (
-      <div className="flex flex-col justify-center">
-        <Header />
-        <div className="pl-80 w-[950px] mt-32 flex flex-col">
-          <h1 className="text-[32px] font-jetbrains font-semibold text-[#17191E] text-end">a Fullstack Developer</h1>
+    <div className="flex flex-col justify-center">
+      <Header />
+      <div className="ml-60 mr-60 sm:ml-4 sm:mr-4 md:ml-12 md:mr-12 lg:ml-28 lg:mr-28 2xl:ml-60 2xl:mr-60">
+      <div className="flex flex-row justify-between">
+        <div className="w-[600px] mt-14 flex flex-col">
           <h1 className="text-[36px] font-jetbrains font-semibold text-white">Bruno Bertholini</h1>
-          <p className="mt-5 text-[16px] font-jetbrains text-[#C7C7C7]">Após ter algumas experiências com Design, me interessei por Front-end e posteriormente me apaixonei por desenvolvimento web fullstack. Projetei e desenvolvi diversos tipos de aplicações, confira.</p>
-          <div className="flex justify-between">
-            <button className="hover:bg-transparent hover:border-white hover:border-[1px] hover:text-white transition-all hover:scale-105 mt-6 text-[#17191E] text-[28px] font-semibold bg-white p-4 pl-8 pr-8 font-jetbrains">entrar em contato</button>
-            <button className="hover:bg-white hover:border-none transition-all hover:scale-105 hover:text-[#17191E] mt-6 text-[#C7C7C7] text-[28px] border-[1px] border-white font-semibold bg-transparent p-4 pl-8 pr-8 font-jetbrains">projetos</button>
+          <h1 className="mt-[-10px] text-[32px] font-jetbrains font-semibold text-[#17191E]">a Fullstack Developer</h1>
+          <p className="mt-5 text-[16px] font-jetbrains text-[#C7C7C7] text-justify">Após ter algumas experiências com Design, me interessei por Front-end e posteriormente me apaixonei por desenvolvimento web fullstack. Projetei e desenvolvi diversos tipos de aplicações, confira.</p>
+          <div className="flex">
+            <button className="hover:opacity-60 transition ease-in-out delay-80 mt-6 text-[#17191E] text-[18px] font-semibold bg-white p-4 pl-8 pr-8 font-jetbrains">entrar em contato</button>
+            <button className="hover:opacity-60 transition ease-in-out delay-80 ml-8 mt-6 text-[#C7C7C7] text-[18px] font-semibold bg-transparent p-4 pl-8 pr-8 font-jetbrains">projetos</button>
           </div>
         </div>
       </div>
+      <div className="pt-32  flex flex-col">
+        <h1 className="text-[36px] pb-12 font-jetbrains font-semibold text-white">Projetos</h1>
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-between">
+        <Project />
+        <Project />
+        </div>
+        <div className="flex mt-20 flex-row justify-between">
+        <Project />
+        <Project />
+        </div>
+      </div>
+    </div>
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,13 @@ export function Header() {
     return (
         <div className="flex justify-center max-w-(1240px) px4 items-center h-24">
             <ul className="flex">
-                <li className="text-white font-jetbrains text-xl p-4 mr-55px hover:text-[#17191E] transition-all cursor-pointer">projetos</li>
-                <li className="text-white font-jetbrains text-xl p-4 hover:text-[#17191E] transition-all cursor-pointer">contato</li>
+                <li className="text-white font-jetbrains text-xl p-4 mr-55px hover:opacity-40 transition-all cursor-pointer">projetos</li>
+                <li className="text-white font-jetbrains text-xl p-4 hover:opacity-40 transition-all cursor-pointer">contato</li>
             </ul>
             <div className="text-white flex ml-55px">
-                <FaGithub size={24} className="mr-8 hover:text-[#17191E] transition-all cursor-pointer" />
-                <FaLinkedin size={24} className="hover:text-[#17191E] transition-all cursor-pointer" />
-                <MdEmail size={24} className="ml-8 hover:text-[#17191E] transition-all cursor-pointer" />
+                <FaGithub size={24} className="mr-8 hover:opacity-40 transition-all cursor-pointer" />
+                <FaLinkedin size={24} className="hover:opacity-40 transition-all cursor-pointer" />
+                <MdEmail size={24} className="ml-8 hover:opacity-40 transition-all cursor-pointer" />
             </div>
         </div>
     )
