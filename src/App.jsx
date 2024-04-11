defad72782573b96bf301d0957adae2de94284f2
Bruno Bertholini <brbertholini@gmail.com>
2024-04-10 22:10:15 -0300

    STYLE: color fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className="flex flex-row justify-between">
         <div className="w-[600px] mt-14 flex flex-col">
           <h1 className="text-[36px] font-jetbrains font-semibold text-white">Bruno Bertholini</h1>
-          <h1 className="mt-[-10px] text-[32px] font-jetbrains font-semibold text-[#17191E]">a Fullstack Developer</h1>
+          <h1 className="mt-[-10px] text-[32px] font-jetbrains font-semibold text-white opacity-30">a Fullstack Developer</h1>
           <p className="mt-5 text-[16px] font-jetbrains text-[#C7C7C7] text-justify">Após ter algumas experiências com Design, me interessei por Front-end e posteriormente me apaixonei por desenvolvimento web fullstack. Projetei e desenvolvi diversos tipos de aplicações, confira.</p>
           <div className="flex">
             <button className="hover:opacity-60 transition ease-in-out delay-80 mt-6 text-[#17191E] text-[18px] font-semibold bg-white p-4 pl-8 pr-8 font-jetbrains">entrar em contato</button>
