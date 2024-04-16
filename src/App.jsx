b4412a6fcbe0054bde1586cce10fbbb9a006a05a
Bruno Bertholini <brbertholini@gmail.com>
2024-04-16 18:47:03 -0300

    section 1 update

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <div className="flex flex-col">
             <h1 className="text-[24px] sm:text-[36px] font-jetbrains font-semibold text-white">Bruno Bertholini,</h1>
             <h1 className="mt-[-10px] text-[24px] sm:text-[32px] font-jetbrains font-semibold text-white">a Fullstack Developer</h1>
-            <p className="mt-5 text-[14px] sm:text-[16px] font-jetbrains text-[#C7C7C7] text-justify">Após ter algumas experiências com Design, me interessei por Front-end e posteriormente me apaixonei por desenvolvimento web fullstack. Projetei e desenvolvi diversos tipos de aplicações, confira.</p>
+            <p className="mt-5 text-[14px] sm:text-[16px] font-jetbrains text-[#C7C7C7] text-justify">Após ter algumas experiências com Design, me interessei por Front-end e posteriormente me apaixonei por desenvolvimento web fullstack, focado no desenvolvimento de soluções que possam facilitar a vida das pessoas. Estou desenvolvendo um projeto acadêmico que conta com um website e um aplicativo mobile voltado para a facilitação do processo de adoção de pets. Além de um projeto paralelo de gestão de estoque.</p>
             <div className="flex flex-col sm:flex-row">
               <button onClick={() => scrollToSection('contact', 140)} className="w-[100%] sm:w-fit hover:opacity-60 hover:scale-110 transition ease-in-out delay-80 mt-6 text-[#17191E] text-[18px] font-semibold bg-white p-4 pl-8 pr-8 font-jetbrains">entrar em contato</button>
               <button onClick={() => scrollToSection('projects', 140)} className="w-[100%] sm:w-fit hover:opacity-60 hover:scale-110 transition ease-in-out delay-80 ml-0 sm:ml-8 mt-6 text-[#C7C7C7] text-[18px] font-semibold bg-transparent p-4 pl-8 pr-8 font-jetbrains">projetos</button>
